cd3185062a4374a3451c8bd589ea84a3f39c227c
Davood Wadi <62418145+davoodwadi@users.noreply.github.com>
2024-08-24 15:49:26 +0330

    adding support for colab HF_TOKENS

--- llm_exceptions/__init__.py
+++ llm_exceptions/__init__.py
@@ -6,10 +6,22 @@ from .llm_exceptions import llm_handler
 from functools import partial
 
 import IPython
+try:
+    # Check for Google Colab
+    import sys
+    if 'google.colab' in sys.modules:
+        print("Running in Google Colab")
+        from google.colab import userdata
+        HF_TOKEN = userdata.get('HF_TOKEN')
+    else:
+        print("Running in Jupyter Notebook")
+        HF_TOKEN = os.environ.get('HF_TOKEN')
+except ImportError:
+    print("Running in another environment")
 
-HF_TOKEN = os.environ.get('HF_TOKEN')
 if not HF_TOKEN:
     raise ValueError("HF_TOKEN is is not added as an environment variable.")
+
 
 def load_ipython_extension(ipython=None, show_html=True):
     if ipython is None:
